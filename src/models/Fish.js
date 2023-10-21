14d66306ec6f2d36b31e594287ea223797738257
Phan Dan <phandan5075@gmail.com>
2023-10-21 15:43:35 +0700

    update slug fish

--- src/models/Fish.js
+++ src/models/Fish.js
@@ -1,13 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const slug = require('mongoose-slug-generator');
+const slug = require("mongoose-slug-generator");
 mongoose.plugin(slug);
 
-const productSchema = new Schema({
-    name: { type: String},
-    
-}, {
-    timestamps: true,
+const priceSchema = new Schema({
+  weight: String, // Ví dụ: "500g", "1kg"
+  price: Number, // Giá cho khối lượng tương ứng
 });
 
-module.exports = mongoose.model('Fish', productSchema)
+const productSchema = new Schema(
+  {
+    name: { type: String },
+    description: { type: String },
+    image: { type: String },
+    prices: [priceSchema],
+    slug: { type: String, slug: "name", unique: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Fish", productSchema);
